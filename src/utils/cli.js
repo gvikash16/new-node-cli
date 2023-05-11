8fd52ae072340671de6af3f3c754ba6a2d2e439c
Rahul Gupta <rahulg.active@gmail.com>
2023-05-11 08:37:25 +0530

    a

---
+++ src/utils/cli.js
@@ -0,0 +1,50 @@
+import chalk from 'chalk';
+import meow from 'meow';
+import config from '../../config/index.js';
+
+const { green, yellow, cyan } = chalk;
+
+const meowHelp = `
+  Usage
+	  ${green(`auth-e2e`)} ${cyan(`<command>`)} ${yellow(`[--option]`)}
+
+	Commands
+	  ${cyan(`setup`)}            Create a config file in root folder
+	  ${cyan(`addPlugin`)}        Add entry to the mounted plugin
+	  ${cyan(`removePlugin`)}     Remove entry from the mounted plugin
+
+	Options
+    ${yellow(`-p`)}, ${yellow(`--preload`)}      To preload the already defined set up
+    ${yellow(`-np`)}, ${yellow(`--no-preload`)}  It will ask you to setup manually
+    ${yellow(`-n`)}, ${yellow(`--project-name`)}  Provide project project-name to the plugin
+
+	Examples
+	  ${green(`auth-e2e`)} ${cyan(`setup`)} ${yellow(`--project-name=`)}${yellow(`"project-name"`)}
+	  ${green(`auth-e2e`)} ${cyan(`addPlugin`)} ${yellow(`--project-name=`)}${yellow(`"plugin-name"`)}
+	  ${green(`auth-e2e`)} ${cyan(`removePlugin`)} ${yellow(`--project-name=`)}${yellow(`"plugin-name"`)}
+`;
+
+const meowFlags = {
+  importMeta: import.meta,
+  default: { projectName: 'en' },
+  flags: {
+    preload: {
+      type: 'boolean',
+      shortFlag: 'p',
+      default: false
+    },
+    branch: {
+      type: 'boolean',
+      shortFlag: 'b',
+      default: false
+    },
+    projectName: {
+      type: 'string',
+      shortFlag: 'p',
+      default: config.defaultProjectName,
+    }
+  
+  }
+}
+
+export default meow(meowHelp, meowFlags);
